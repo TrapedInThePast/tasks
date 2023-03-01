3d9de1ff2c82bbaff879c7be1dc21f7bdc62fac9
Emma Metcalfe <emmreese@udel.edu>
2023-03-01 10:51:58 -0500

    Completed the function countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -73,7 +73,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const short = words.filter((word: string): boolean => word.length < 4);
+    return short.length;
 }
 
 /**
