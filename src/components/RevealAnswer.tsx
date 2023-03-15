a4086fdbd290f4f1d636852fffe07874ef31333e
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 16:41:09 -0400

    Made state to handle whether the text is visible

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false);
     return (
         <div>
             <Button>Reveal Answer</Button>
