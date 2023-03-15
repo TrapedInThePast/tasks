2575f51a9dfbf7696ae7ec1c735f833a778ae340
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 17:26:31 -0400

    Made current type visible in text

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -6,7 +6,24 @@ export function ChangeType(): JSX.Element {
     const [value, setValue] = useState<QuestionType>("short_answer_question");
     return (
         <div>
-            <Button>Change Type</Button>
+            <Button
+                onClick={() =>
+                    setValue(
+                        value === "short_answer_question"
+                            ? "multiple_choice_question"
+                            : "short_answer_question"
+                    )
+                }
+            >
+                Change Type
+            </Button>
+            to{" "}
+            {value === "multiple_choice_question" ? (
+                <div>Multiple Choice</div>
+            ) : (
+                <div>Short Answer</div>
+            )}{" "}
+            .
         </div>
     );
 }
