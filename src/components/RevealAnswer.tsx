1c6827716e13cf13f87b5ee5bb2941632d212568
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 16:36:21 -0400

    Made Reveal Answer Button

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,9 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    return (
+        <div>
+            <Button>Reveal Answer</Button>
+        </div>
+    );
 }
