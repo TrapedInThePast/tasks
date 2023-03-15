73cb15821ae142235c63359370c154848cd7fc2a
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 17:48:43 -0400

    Made Start Quiz Button

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -4,5 +4,16 @@ import { Button } from "react-bootstrap";
 export function StartAttempt(): JSX.Element {
     const [attempts, setAttempts] = useState<number>(4);
     const [progress, setProgress] = useState<boolean>(false);
-    return <div>Start Attempt</div>;
+    return (
+        <span>
+            <Button
+                onClick={() => {
+                    setProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+            >
+                Start Quiz
+            </Button>
+        </span>
+    );
 }
