2914ac9f364c4b823923848f2d2945ee71b2fce1
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 17:38:36 -0400

    Made initial state quiz not in progress

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -3,6 +3,6 @@ import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
     const [attempts, setAttempts] = useState<number>(4);
-
+    const [progress, setProgress] = useState<boolean>(false);
     return <div>Start Attempt</div>;
 }
