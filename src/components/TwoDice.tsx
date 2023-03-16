0b50601cea2caf0a19ef3c77c0f9785246e5d71e
Emma Metcalfe <emmreese@udel.edu>
2023-03-15 20:44:51 -0400

    Made Roll Right Button

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -18,6 +18,7 @@ export function TwoDice(): JSX.Element {
         <div>
             <Button onClick={() => setDice1(d6())}>Roll Left</Button>
             <span data-testid="left-die">die one: {dice1}</span>
+            <Button onClick={() => setDice2(d6())}>Roll Right</Button>
         </div>
     );
 }
